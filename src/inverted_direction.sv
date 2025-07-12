9317ec4373dee07eb5edd478b6c7c0f0ba2b74d9
Ahmet Yilmaz <yilmazahmet547@gmail.com>
2025-07-12 21:06:23 +0300

    inverted direction toparlandı, fifo değişecek

--- src/inverted_direction.sv
+++ src/inverted_direction.sv
@@ -1,138 +1,112 @@
 `timescale 1ns / 1ps
 `include "Types.sv"
 
-    // ESKI FSM MANTIGIYLA YAPILMISTI
+// 16 DIV cluster, 48 tag size ile bu modul
+// 11706 LUT 13418 FF 0 DSP kullanıyor
 module inverted_direction #(
     parameter WIDTH = `WIDTH,
     parameter Q_BITS = `Q_BITS,
-    parameter MAX = `MAX_16,
-    parameter MIN = `MIN_16
+    parameter TAG_SIZE = `TAG_SIZE,
+    parameter DIV_COUNT = 16
     )
     (
     input clk,
     input start,
     input reset,
-    input RayDirection norm_dir,
-    output state state_out,
+    input TaggedDirection direction_in,
     output logic valid_out,
-    output InvertedRayDirection inv_dir
+    output TaggedDirection inv_dir_out
     );
     
-    logic valid_x, valid_y, valid_z;
-    state current_state_x;
-    state current_state_y;
-    state current_state_z;
-    state current_state;
+    TaggedDirection tagged_direction;
+    assign tagged_direction = direction_in;
     
-    wire InvertedRayDirection inv;
-    
-    
-    // giris vektoru (0,0,0) ise atla
-    wire skip;
-    assign skip = ~|(norm_dir.x | norm_dir.y | norm_dir.z);
-    
-    wire skip_x;
-    wire skip_y;
-    wire skip_z;
-    assign skip_x = ~|(norm_dir.x);
-    assign skip_y = ~|(norm_dir.y);
-    assign skip_z = ~|(norm_dir.z);
-    
-    wire start_real_x;
-    wire start_real_y;
-    wire start_real_z;
-    assign start_real_x = start & !skip_x;
-    assign start_real_y = start & !skip_y;
-    assign start_real_z = start & !skip_z;
-    
-    wire valid_skip_x;
-    wire valid_skip_y;
-    wire valid_skip_z;
-    assign valid_skip_x = skip_x;
-    assign valid_skip_y = skip_y;
-    assign valid_skip_z = skip_z;
-    
-    //wire start_real;
-    //assign start_real = start & !skip;
-    state div_state_x,div_state_y,div_state_z;
+    TaggedDirection TD_fifo_out;
+    logic TD_buffer_ready;
+    logic TD_buffer_valid;
+    logic read_in;
     
     
-    float_divider #(
-        .WIDTH(WIDTH),
-        .Q_BITS(Q_BITS)
-    )divX(
-        .clk(clk),
-        .reset(reset),
-        .start(start_real_x),
-        .dividend($signed({4'h1,{WIDTH-4{1'b0}}})),
-        .divisor(norm_dir.x),
-        .quotient(inv.x),
-        .div_state(div_state_x),
-        .valid(valid_x)
+    
+    TD_fifo #(
+    .DEPTH(24),
+    .TAG_SIZE(TAG_SIZE)
+    ) TD_buffer (
+    .clk(clk),
+    .reset(reset),
+    .read(read_in),
+    .write(start),
+    .TD_in(tagged_direction),
+    .TD_out(TD_fifo_out),
+    .ready(TD_buffer_ready),
+    .overflow(),
+    .valid(TD_buffer_valid)
     );
     
-    float_divider #(
-        .WIDTH(WIDTH),
-        .Q_BITS(Q_BITS)
-    )divY(
-        .clk(clk),
-        .start(start_real_x),
-        .reset(reset),
-        .dividend($signed({4'h1,{WIDTH-4{1'b0}}})),
-        .divisor(norm_dir.y),
-        .div_state(div_state_y),
-        .quotient(inv.y),
-        .valid(valid_y)
+    wire [DIV_COUNT-1:0]div_req;        // bosta bulunan divider portlari
+    wire [DIV_COUNT-1:0]div_gnt;        // izin verilen divider portlari
+    wire [DIV_COUNT-1:0]div_gnt_start;  // start sinyali verilen divider portlari
+    
+    
+    round_robin_arbiter #(
+    .NUM_PORTS(DIV_COUNT)
+    )div_arbiter (
+    .clk(clk),
+    .reset(reset),
+    .req_i(div_req),
+    .gnt_o(div_gnt)
     );
-        
-    float_divider #(
-        .WIDTH(WIDTH),
-        .Q_BITS(Q_BITS)
-    )divZ(
-        .clk(clk),
-        .start(start_real_z),
-        .reset(reset),
-        .dividend($signed({4'h1,{WIDTH-4{1'b0}}})),
-        .divisor(norm_dir.z),
-        .div_state(div_state_z),
-        .quotient(inv.z),
-        .valid(valid_z)
-        );
-        
-    logic valid_all;
-    assign valid_all = (valid_x | valid_skip_x) & (valid_y | valid_skip_y) & (valid_z  | valid_skip_z);
+    // buffer ready ise ve istekte bulunan divider var ise fifo'dan oku
+    assign read_in = |div_req & TD_buffer_ready;
     
-    assign current_state_x = (div_state_x == BUSY | div_state_x == WAITING) ? BUSY : IDLE;
-    assign current_state_y = (div_state_y == BUSY | div_state_y == WAITING) ? BUSY : IDLE;
-    assign current_state_z = (div_state_z == BUSY | div_state_z == WAITING) ? BUSY : IDLE;
-    assign current_state =  (current_state_x == BUSY | current_state_y == BUSY | current_state_z == BUSY) ? BUSY : IDLE;
+    logic fifo_overflow[DIV_COUNT-1:0];
     
-    always_ff @(posedge clk or posedge reset or posedge skip) begin
-        if (reset | skip) begin
-            inv_dir.x <= 0;
-            inv_dir.y <= 0;
-            inv_dir.z <= 0;
-            state_out <= IDLE;
-        end else if(valid_all) begin
-            if(valid_skip_x) begin
-                inv_dir.x <= MAX;
-            end else begin
-                inv_dir.x <= inv.x;
-            end
-            if(valid_skip_y) begin
-                inv_dir.y <= MAX;
-            end else begin
-                inv_dir.y <= inv.y;
-            end
-            if(valid_skip_z) begin
-                inv_dir.z <= MAX;
-            end else begin
-                inv_dir.z <= inv.z;
-            end
-            
-            valid_out <= valid_all;
-        end else begin
-            state_out <= current_state;
-        end
+    // div'lerin start portunun baglantisini yap
+    genvar j;
+        for(j = 0; j < DIV_COUNT; j++) begin
+            assign div_gnt_start[j] = TD_buffer_valid & div_gnt[j] & ~fifo_overflow[j];
     end
+    
+    logic [DIV_COUNT-1:0]div_valid;
+    TaggedDirection inverted[DIV_COUNT-1:0];
+    
+    
+    // XYZ divider'lari olustur
+    genvar i;
+    generate
+        for(i = 0; i < DIV_COUNT; i++) begin
+            inv_div_cluster #(
+            .WIDTH(WIDTH),
+            .Q_BITS(Q_BITS)
+            ) divXYZ(
+            .clk(clk),
+            .start(div_gnt_start[i]),
+            .reset(reset),
+            .TD_in(TD_fifo_out),
+            .valid(div_valid[i]),
+            .ready(div_req[i]),
+            .ITD_out(inverted[i])
+            );
+        end
+    endgenerate
+    
+    logic fifo_valid;
+    TaggedDirection tagged_fifo_out;
+    // out-of-order cikan div sonuclarini sirayla cikarmak icin sorted fifo
+    sorted_fifo #(
+    .DEPTH(32),
+    .TAG_SIZE(TAG_SIZE),
+    .DIV_COUNT(DIV_COUNT)
+    ) sorted_fifo (
+    .clk(clk),
+    .reset(reset),
+    .div_valid_in(div_valid),
+    .tagged_norm_in(inverted),
+    .fifo_overflow_out(fifo_overflow),
+    .tagged_norm_out(tagged_fifo_out),
+    .valid_out(fifo_valid)
+    );
+    
+    assign inv_dir_out = tagged_fifo_out;
+    assign valid_out = fifo_valid;
 endmodule
