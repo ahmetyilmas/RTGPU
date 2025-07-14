106a06e36b9e462462b98971d8f284da50133e6b
Ahmet Yilmaz <ahmetyilmas03@gmail.com>
2025-07-14 19:24:23 +0300

    BRAM olarak sentezlendi

--- src/util/fifo.sv
+++ src/util/fifo.sv
@@ -13,50 +13,58 @@ module fifo#(
     output logic [WIDTH-1:0] data_out,
     output logic ready,
     output logic overflow,
-    output logic valid
+    output logic valid_out
     );
     
     
-    logic [WIDTH-1:0]fifo_buffer[DEPTH-1:0];
-    logic [$clog2(DEPTH)-1:0] write_pointer;
-    logic [$clog2(DEPTH)-1:0] read_pointer;
+    (*ram_style = "block" *) logic [WIDTH-1:0] fifo [0:DEPTH-1];
+
+    logic valid;
+    logic [WIDTH-1:0] out;
+    
+    logic [$clog2(DEPTH)-1:0] wptr;
+    logic [$clog2(DEPTH)-1:0] rptr;
     logic [$clog2(DEPTH+1)-1:0] fifo_count;
     
     always_ff @(posedge clk or posedge reset) begin
         if(reset) begin
-            write_pointer <= 0;
-            read_pointer <= 0;
+            wptr <= 0;
+            rptr <= 0;
             fifo_count <= 0;
             overflow <= 0;
             valid <= 0;
         end else begin
-            overflow <= 0;
-            valid <= 0;
-            if(write && !read && fifo_count < DEPTH) begin
-                fifo_buffer[write_pointer] <= data_in;       //datayı yaz
-                write_pointer <= (write_pointer + 1) % DEPTH;//pointer++
-                fifo_count <= fifo_count + 1;                // count++
-            end else if(write && fifo_count == DEPTH) begin
-                overflow <= 1;
-            end 
-            if(read && !write && fifo_count > 0) begin
-                data_out <= fifo_buffer[read_pointer];      // datayi oku
-                read_pointer <= (read_pointer + 1) % DEPTH; // pointer++
-                fifo_count <= fifo_count - 1;               // count--
-                valid <= 1;             
-            end
-            
-            if(read && write && fifo_count < DEPTH && fifo_count > 0) begin
-                data_out <= fifo_buffer[read_pointer];
-                fifo_buffer[write_pointer] <= data_in;
-                read_pointer <= (read_pointer + 1) % DEPTH;
-                write_pointer <= (write_pointer + 1) % DEPTH;
-                valid <= 1; 
+            if(write && read && fifo_count > 0 && fifo_count < DEPTH) begin
+                    wptr <= (wptr + 1) % DEPTH;
+                    rptr <= (rptr + 1) % DEPTH;
+                    valid <= 1;
+            end else begin
+                if(write && fifo_count < DEPTH) begin
+                   wptr <= (wptr + 1) % DEPTH;
+                   fifo_count <= fifo_count + 1;
+                end
                 
+                if(read && fifo_count > 0) begin
+                    rptr    <= (rptr + 1 ) % DEPTH;
+                    fifo_count <= fifo_count - 1;
+                    valid   <= 1;
+                    overflow <= 0;
+                end
             end
-            
+        end
+        overflow <= (fifo_count == DEPTH);
+    end
+
+    always_ff @(posedge clk) begin
+        if(write) begin
+            fifo[wptr]   <=  data_in;
+        end
+        if(read) begin
+            out   <= fifo[rptr];
         end
     end
     
     assign ready = (fifo_count > 0);
+    assign data_out = out;
+    assign valid_out = valid;
 endmodule
