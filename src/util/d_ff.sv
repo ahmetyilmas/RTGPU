d5dccc58309aac1204a30951a217821ce32fc0a1
Ahmet Yilmaz <ahmetyilmas03@gmail.com>
2025-07-19 17:46:06 +0300

    initial commit

---
+++ src/util/d_ff.sv
@@ -0,0 +1,23 @@
+`timescale 1ns/1ps
+
+module d_ff#(
+    WIDTH = `WIDTH
+)(
+    input clk,
+    input reset,
+    input logic [WIDTH-1:0]d_in,
+    output logic [WIDTH-1:0]q_out
+);
+    logic [WIDTH-1:0]q_ff;
+
+    always_ff @(posedge clk) begin
+        if(reset) begin
+            q_ff <= 0;
+        end else begin
+            q_ff <= d_in;
+        end
+    end
+
+    assign q_out = q_ff;
+    
+endmodule
