ca39ed642e25d064fb3d90fe1839a5478324ad15
Ahmet Yilmaz <ahmetyilmas03@gmail.com>
2025-07-19 19:19:08 +0300

    Yön vektörlerinin karesini alan modül.

---
+++ src/util/direction_square.sv
@@ -0,0 +1,58 @@
+`timescale 1ns/1ps
+`include "Types.sv"
+
+module direction_square #(
+    parameter WIDTH  = `WIDTH,
+    parameter Q_BITS = `Q_BITS,
+)(
+    input clk,
+    input start,
+    input RayDirection RD_in,
+    output logic valid,
+    output RayDirection_sqr RDS_out
+);
+
+    logic signed [WIDTH-1:0]x;
+    logic signed [WIDTH-1:0]y;
+    logic signed [WIDTH-1:0]z;
+    
+    logic signed [2*WIDTH-1:0]sqr_x;
+    logic signed [2*WIDTH-1:0]sqr_y;
+    logic signed [2*WIDTH-1:0]sqr_z;
+    
+    assign x = RD_in.x;
+    assign y = RD_in.y;
+    assign z = RD_in.z;
+    
+    assign sqr_x = x * x;
+    assign sqr_y = y * y;
+    assign sqr_z = z * z;
+    
+    always_ff @(posedge clk) begin
+        if (start) begin
+            RDS_out = '{
+                x : RD_in.x,
+                y : RD_in.y,
+                z : RD_in.z,
+
+                sqr_x : sqr_x,
+                sqr_y : sqr_y,
+                sqr_z : sqr_z
+            };
+            valid <= 1;
+        end else begin
+            RDS_out = '{
+                x : 0,
+                y : 0,
+                z : 0,
+
+                sqr_x : 0,
+                sqr_y : 0,
+                sqr_z : 0
+            };
+            valid <= 0;
+        end
+    end
+
+    
+endmodule
