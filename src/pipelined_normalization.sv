c3fe7dc24e5b84bdde8c2e2b48724c45b69e6c33
Ahmet Yilmaz <ahmetyilmas03@gmail.com>
2025-07-20 14:50:42 +0300

    Tagsız hale getirildi, TB yapıldı ve sentezlendi.

--- src/pipelined_normalization.sv
+++ src/pipelined_normalization.sv
@@ -1,8 +1,11 @@
 `timescale 1ns / 1ps
 `include "Types.sv"
 /*
- 48 TAG_SIZE 16 Div cluster için
- Bu modül 12639 LUT, 11286 FF, 5.50 BRAM ve 3 DSP kullanıyor
+    16 WIDTH, 12 Q_BITS ve 20 DEPTH icin sentez sonuclari
+    LUT:    5012
+    FF:     3881
+    BRAM:   1.50
+    LUTRAM: 80
 */
     // len = sqrt(x^2 + y^2 + z^2)
     // L'x = Lx / len
@@ -11,8 +14,8 @@
 module pipelined_normalization #(
     parameter WIDTH = `WIDTH,
     parameter Q_BITS = `Q_BITS,
-    parameter DIV_COUNT = 16,
-    parameter TAG_SIZE = `TAG_SIZE
+    parameter MAX = `MAX_16,
+    parameter MIN = `MIN_16
     )(
     input clk,
     input reset,
@@ -25,7 +28,7 @@ module pipelined_normalization #(
     logic mul_valid;
     
     // isin yonlerinin kareleri
-    Vec3 RD_square;
+    RayDirection_sqr RD_square;
     
     direction_square #(
     .WIDTH(WIDTH),
@@ -34,120 +37,50 @@ module pipelined_normalization #(
     .clk(clk),
     .start(start),
     .RD_in(ray_in),
-    .valid(mul_valid),
+    .valid_out(mul_valid),
     .RDS_out(RD_square)
     );
     
     wire [WIDTH-1:0]sum;
-    assign sum = RD_square.x + RD_square.y + RD_square.z;
-    
+    RayDirection RD_sqrt_in;
+
+    assign sum = RD_square.sqr_x + RD_square.sqr_y + RD_square.sqr_z;
+    assign RD_sqrt_in = '{
+        x : RD_square.x,
+        y : RD_square.y,
+        z : RD_square.z
+    };
+
     logic sqrt_valid;
     RayDirection_len RD_len;
     
-    // BURADAYIM
-    
-    sqroot_tagged #(
+    direction_sqroot #(
     .WIDTH(WIDTH),
-    .Q_BITS(Q_BITS)
+    .Q_BITS(Q_BITS),
+    .DEPTH(20)
     ) sqrt_core (
     .clk(clk),
     .start(mul_valid),
     .reset(reset),
-    .x_in(sum),
-    .TD_in(RD_square),
+    .sum_in(sum),
+    .RD_in(RD_sqrt_in),
     .valid_out(sqrt_valid),
-    .TDL_out(RD_len)
+    .RDLEN_out(RD_len),
+    .overflow_out()
     );
     
-    logic sqrt_buffer_ready;
-    logic sqrt_buffer_valid;
-    logic read_in;
-    TaggedDirection_len TDL_fifo_out;
-    
-    // Tag + Direction + len fifo
-    tdl_dp_fifo #(
-    .WIDTH(WIDTH),
-    .DEPTH(32),
-    .TAG_SIZE(TAG_SIZE)
-    ) sqrt_buffer (
-    .clk(clk),
-    .reset(reset),
-    .read(read_in),
-    .write(sqrt_valid),
-    .TDL_in(TDL_sqrt),
-    .TDL_out(TDL_fifo_out),
-    .ready(sqrt_buffer_ready),
-    .overflow(),
-    .valid_out(sqrt_buffer_valid)
+    NR_div_block #(
+        .WIDTH(WIDTH),
+        .Q_BITS(Q_BITS),
+        .MAX(MAX),
+        .MIN(MIN)
+    ) NR_division (
+        .clk(clk),
+        .reset(reset),
+        .start(sqrt_valid),
+        .RDL_in(RD_len),
+        .valid_out(valid_out),
+        .normalized_ray_out(normalized_ray_out)
     );
     
-    TaggedDirection_len TDL_div_in;
-    assign TDL_div_in = TDL_fifo_out;
-    wire [DIV_COUNT-1:0]div_req;        // bosta bulunan divider portlari
-    wire [DIV_COUNT-1:0]div_gnt;        // izin verilen divider portlari
-    wire [DIV_COUNT-1:0]div_gnt_start;  // start sinyali verilen divider portlari
-    
-    
-    round_robin_arbiter #(
-    .NUM_PORTS(DIV_COUNT)
-    )div_arbiter (
-    .clk(clk),
-    .reset(reset),
-    .req_i(div_req),
-    .gnt_o(div_gnt)
-    );
-
-    // buffer ready ise ve istekte bulunan divider var ise fifo'dan oku
-    assign read_in = |div_req & sqrt_buffer_ready;
-    
-    logic fifo_overflow[DIV_COUNT-1:0];
-    
-    // div'lerin start portunun baglantisini yap
-    genvar j;
-    for(j = 0; j < DIV_COUNT; j++) begin
-        assign div_gnt_start[j] = sqrt_buffer_valid & div_gnt[j] & ~fifo_overflow[j];
-    end
-    
-    
-    logic [DIV_COUNT-1:0]div_valid;
-    TaggedNormalized normalized[DIV_COUNT-1:0];
-    
-    // XYZ divider'lari olustur
-    genvar i;
-    generate
-        for(i = 0; i < DIV_COUNT; i++) begin
-            div_cluster #(
-            .WIDTH(WIDTH),
-            .Q_BITS(Q_BITS)
-            ) divXYZ(
-            .clk(clk),
-            .start(div_gnt_start[i]),
-            .reset(reset),
-            .TDL_in(TDL_fifo_out),
-            .valid(div_valid[i]),
-            .ready(div_req[i]),
-            .normalized(normalized[i])
-            );
-        end
-    endgenerate
-    
-    
-    logic fifo_valid;
-    TaggedNormalized tagged_fifo_out;
-    // out-of-order cikan div sonuclarini sirayla cikarmak icin sorted fifo
-    sorted_fifo #(
-    .DEPTH(32),
-    .TAG_SIZE(TAG_SIZE),
-    .DIV_COUNT(DIV_COUNT)
-    ) sorted_fifo (
-    .clk(clk),
-    .reset(reset),
-    .div_valid_in(div_valid),
-    .tagged_norm_in(normalized),
-    .fifo_overflow_out(fifo_overflow),
-    .tagged_norm_out(tagged_fifo_out),
-    .valid_out(fifo_valid)
-    );
-    assign normal = tagged_fifo_out;
-    assign valid_out = fifo_valid;
 endmodule
