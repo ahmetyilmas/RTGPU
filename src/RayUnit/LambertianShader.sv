f794586490162a5e327e8ad0fad0afca9c93ca61
Ahmet Yilmaz <ahmetyilmas03@gmail.com>
2025-07-25 19:11:12 +0300

    initial commit

---
+++ src/RayUnit/LambertianShader.sv
@@ -0,0 +1,15 @@
+`include "../Types.sv"
+`include "../Parameters.sv"
+
+module LambertianShader #(
+    parameter int WIDTH = 20,
+    parameter int Q_BITS = 12
+)(
+    input clk,
+    input reset,
+    input start,
+    input Color_t color_in
+
+);
+
+endmodule
