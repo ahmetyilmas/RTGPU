75cf5273db5a6700fd734c13e8e395cf008da872
Ahmet Yilmaz <ahmetyilmas03@gmail.com>
2025-07-25 19:09:48 +0300

    Vec3_t eklendi.

--- src/Types.sv
+++ src/Types.sv
@@ -11,6 +11,12 @@ typedef struct packed {
     logic signed [`WIDTH-1:0] x;
     logic signed [`WIDTH-1:0] y;
     logic signed [`WIDTH-1:0] z;
+} Vec3_t;
+
+typedef struct packed {
+    logic signed [`WIDTH-1:0] x;
+    logic signed [`WIDTH-1:0] y;
+    logic signed [`WIDTH-1:0] z;
     logic [`TAG_SIZE-1:0] tag;
 } TaggedVec3;
 
@@ -104,11 +110,11 @@ typedef struct packed {
 } TaggedInvertedRayDirection;
 
 typedef enum logic[1:0]{
-    BUSY =      2'b00,  // birim bir veriyi isliyor ve yeni veri alamaz
-    IDLE =      2'b01,  // birim bosta duruyor yeni veri kabul edebilir
-    WAITING =   2'b10,  // birim mevcut islemi bitirmis ama sonraki cikis bekliyor
-    ACCEPTING = 2'b11   // birim bir veri isliyor ama yeni veri alabilir
-}state;
+    S0_BUSY =      2'b00,  // birim bir veriyi isliyor ve yeni veri alamaz
+    S1_IDLE =      2'b01,  // birim bosta duruyor yeni veri kabul edebilir
+    S2_WAITING =   2'b10,  // birim mevcut islemi bitirmis ama sonraki cikis bekliyor
+    S3_ACCEPTING = 2'b11   // birim bir veri isliyor ama yeni veri alabilir
+}state_t;
 
 typedef struct packed {
     logic [7:0]r;
@@ -133,11 +139,41 @@ typedef struct packed {
 } AABB_result;
 
 typedef struct packed {
+    AABB box;
+    logic ray_hit;
+    logic [`WIDTH-1:0] tmin;
+    Vec3_t normal;
+} AABB_result_t;
+
+typedef struct packed {
+    AABB box;
+    logic ray_hit;
+    logic [`WIDTH-1:0] tmin;
+    Vec3_t normal;
+} Intersection_result_t;
+
+
+typedef struct packed {
     RayOrigin origin;                           // kamera konumu
     Vec3 forward;                               // bakis yonu
     Vec3 up;                                    // yukari bakis yonu
     logic signed [`WIDTH-1:0] fov;              // gorus acisi
     logic signed [`WIDTH-1:0] aspect_ratio;     // genislik/yukseklik orani
 }Camera;
+
+typedef struct packed {
+    RayOrigin origin;                           // kamera konumu
+    Vec3_t forward;                             // bakis yonu
+    Vec3_t up;                                  // yukari bakis yonu
+    logic signed [`WIDTH-1:0] fov;              // gorus acisi
+    logic signed [`WIDTH-1:0] aspect_ratio;     // genislik/yukseklik orani
+}Camera_t;
+
+typedef struct {
+  logic [31:0] addr;
+  logic [31:0] data;
+  logic        valid;
+  logic        write_en;
+} axi_transaction_t;
 
 `endif
