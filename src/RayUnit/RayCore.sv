296a464a7ec87e24490c2e9b9ee58e526318be88
Ahmet Yilmaz <ahmetyilmas03@gmail.com>
2025-08-05 16:49:06 +0300

    initial commit

---
+++ src/RayUnit/RayCore.sv
@@ -0,0 +1,17 @@
+`timescale 1ns/1ps
+
+`include "Types.sv"
+`include "Parameters.sv"
+
+
+module RayCore #(
+    parameter int WIDTH = 20,
+    parameter int QBITS = 12
+) (
+    input clk,
+    input reset,
+    input start,
+    input Cam_t camera_in
+);
+
+endmodule
